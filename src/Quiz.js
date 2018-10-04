1d0cc2404f263bb18ced24fc4b0baea0ccfe63f4
owenbob <30471763+owenbob@users.noreply.github.com>
2018-10-04 12:02:44 +0300

    add quiz reset (#9)

--- src/Quiz.js
+++ src/Quiz.js
@@ -11,6 +11,10 @@ class Quiz extends Component {
     this.state = { quiz_position: 1 };
   }
 
+  handleResetClick() {
+    this.setState({ quiz_position: 1 });
+  }
+
   showNextQuestion() {
     this.setState(state => ({ quiz_position: this.state.quiz_position + 1 }));
   }
@@ -19,7 +23,7 @@ class Quiz extends Component {
     const isQuizEnd = ((this.state.quiz_position - 1) === quizData.quiz_questions.length);
     return (
       <div>
-        {isQuizEnd ? <QuizEnd />
+        {isQuizEnd ? <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} />
           : (
             <QuizQuestion
               quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}
